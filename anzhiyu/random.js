dd056ff6834a53a1b68c29099c2c191b13b376c3
beeeeeeef <2113139341@qq.com>
2026-02-20 00:40:05 +0800

    Site updated: 2026-02-20 00:40:05

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2026/02/19/hello-world/"];function toRandomPost(){
+var posts=["2026/02/19/第一次配置博客/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
