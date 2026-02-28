a106d56ca0247b14a2e204194731aa202d8f6ab9
beeeeeeef <2113139341@qq.com>
2026-02-28 21:22:05 +0800

    Site updated: 2026-02-28 21:22:04

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2026/02/19/第一次配置博客/"];function toRandomPost(){
+var posts=["2026/02/19/第一次配置博客/","2026/02/28/通俗易懂的盒子模型/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
